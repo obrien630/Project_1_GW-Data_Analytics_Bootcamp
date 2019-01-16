e2704516a81a7f3ae30dec1d7ba1a19fc0a7db2f
obrien630 <obrien630@gmail.com>
2019-01-15 21:43:26 -0500

    New gitignore file

--- Project 1 Jupyter Notebook/config.py
+++ Project 1 Jupyter Notebook/config.py
@@ -1,4 +1,1 @@
-nyt_api_key = "33fe76b8ce754bd2a25e051d19cc301c"
-weather_api_key = "204f0c83abf7c1084d645d443963167b"
 gkey = "AIzaSyDtl3IdC1y6B3r-1lBIwxfbpEnEGDSHSY0"
-census_api_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
